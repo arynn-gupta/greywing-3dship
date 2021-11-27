5a3fc38042b1b40f8fa3c1414c1b8fca9db5b132
Aryan Gupta <arynn.gupta@gmail.com>
2021-11-27 13:37:26 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import './ship.js';
 
 function App() {
+  var screenWidth;
+  var screenHeight;
+
+  init();
+
+  function init() {
+    screenWidth = window.innerWidth;
+    screenHeight = window.innerHeight;
+  }
+
+  var maxDegrees = 90;
+
+  window.onmousemove = function (event) {
+    var mouseX = event.pageX / screenWidth;
+    var mouseY = event.pageY / screenHeight;
+    var yDegrees = mouseX * maxDegrees - 0.5 * maxDegrees;
+    var xDegrees = -0.5 * (mouseY * maxDegrees - 0.5 * maxDegrees);
+
+    document.getElementsByClassName('logo')[0].style.transform =
+      'rotateY(' + yDegrees + 'deg) rotateX(' + xDegrees + 'deg)';
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='container'>
+        Grey<div className='logo'>W</div>ing
+      </div>
     </div>
   );
 }
---
+++ src/ship.js
@@ -0,0 +1,162 @@
+import * as THREE from 'three';
+
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import { Water } from 'three/examples/jsm/objects/Water.js';
+import { Sky } from 'three/examples/jsm/objects/Sky.js';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import * as dat from 'dat.gui';
+
+let camera, scene, renderer;
+let controls, water, sun;
+let currentPos=0;
+
+const canvas = document.querySelector('canvas.webgl');
+// const gui = new dat.GUI();
+
+const loader = new GLTFLoader();
+
+class Boat {
+  constructor() {
+    loader.load('assets/boat/scene.gltf', (gltf) => {
+      scene.add(gltf.scene);
+      gltf.scene.scale.set(50, 50, 50);
+      gltf.scene.position.set(5, 13, 50);
+      gltf.scene.rotation.y = 1.54;
+      this.boat = gltf.scene;
+    });
+  }
+
+  update() {
+    if (this.boat) {
+      console.log(this.boat.rotation.y);
+      this.boat.rotation.y = 2.5-currentPos;
+      this.boat.position.z -= 1;
+    }
+  }
+}
+
+const boat = new Boat();
+
+init();
+animate();
+
+async function init() {
+  renderer = new THREE.WebGLRenderer({
+    canvas: canvas,
+  });
+  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.toneMapping = THREE.ACESFilmicToneMapping;
+
+  scene = new THREE.Scene();
+
+  camera = new THREE.PerspectiveCamera(
+    55,
+    window.innerWidth / window.innerHeight,
+    1,
+    20000
+  );
+  camera.position.set(-20, 20, 150);
+  // const cameraPos = gui.addFolder('cameraPos');
+  // cameraPos.add(camera.position, 'x').min(-1000).max(1000).step(0.1);
+  // cameraPos.add(camera.position, 'y').min(-1000).max(1000).step(0.1);
+  // cameraPos.add(camera.position, 'z').min(-1000).max(1000).step(0.1);
+  // cameraPos.open();
+
+  // const cameraRot = gui.addFolder('cameraRot');
+  // cameraRot.add(camera.rotation, 'x').min(0).max(1000).step(0.1);
+  // cameraRot.add(camera.rotation, 'y').min(0).max(1000).step(0.1);
+  // cameraRot.add(camera.rotation, 'z').min(0).max(1000).step(0.1);
+  // cameraRot.open();
+
+  sun = new THREE.Vector3();
+
+  // Water
+
+  const waterGeometry = new THREE.PlaneGeometry(10000, 10000);
+
+  water = new Water(waterGeometry, {
+    textureWidth: 512,
+    textureHeight: 512,
+    waterNormals: new THREE.TextureLoader().load(
+      'assets/waternormals.jpg',
+      function (texture) {
+        texture.repeat.set(512, 512);
+        texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
+      }
+    ),
+    sunDirection: new THREE.Vector3(),
+    sunColor: 0xffffff,
+    waterColor: 0x001e0f,
+    distortionScale: 3.7,
+    fog: scene.fog !== undefined,
+  });
+
+  water.rotation.x = -Math.PI / 2;
+
+  scene.add(water);
+
+  // Skybox
+
+  const sky = new Sky();
+  sky.scale.setScalar(10000);
+  scene.add(sky);
+
+  const skyUniforms = sky.material.uniforms;
+
+  skyUniforms['turbidity'].value = 10;
+  skyUniforms['rayleigh'].value = 2;
+  skyUniforms['mieCoefficient'].value = 0.005;
+  skyUniforms['mieDirectionalG'].value = 0.8;
+
+  const parameters = {
+    elevation: 2,
+    azimuth: 180,
+  };
+
+  const pmremGenerator = new THREE.PMREMGenerator(renderer);
+
+  function updateSun() {
+    const phi = THREE.MathUtils.degToRad(90 - parameters.elevation);
+    const theta = THREE.MathUtils.degToRad(parameters.azimuth);
+
+    sun.setFromSphericalCoords(1, phi, theta);
+
+    sky.material.uniforms['sunPosition'].value.copy(sun);
+    water.material.uniforms['sunDirection'].value.copy(sun).normalize();
+
+    scene.environment = pmremGenerator.fromScene(sky).texture;
+  }
+
+  updateSun();
+
+  controls = new OrbitControls(camera, renderer.domElement);
+  controls.enableZoom = false;
+  controls.enableRotate = false;
+  controls.enablePan = false;
+  controls.update();
+
+  window.addEventListener('resize', onWindowResize);
+
+  window.addEventListener('mousemove', function (e) {
+      currentPos = e.pageX / 1000;
+  });
+}
+
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+}
+
+function animate() {
+  requestAnimationFrame(animate);
+  render();
+  boat.update();
+  camera.position.z -= 1;
+}
+
+function render() {
+  water.material.uniforms['time'].value += 1.0 / 60.0;
+  renderer.render(scene, camera);
+}
